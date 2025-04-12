9e51875027c68430e314abc16037a18708da7809
Vishwa1112 <boxervishwa1@gmail.com>
2025-04-12 08:21:48 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,11 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+e=int(input("enter a number"))
+if a>b and a>b and a>d and a>e:
+    print(a," is greater than ",b,"and",c,"and",d,"and",e)
+elif b>a and b>c and b>d and b>e:
+    print(b,"is greater than ",a,"and",c,"and",d,"and",e)
+else:
+    print(c,"is greater than ",a, "and",b,"and",c,"and",d,"and",e)
