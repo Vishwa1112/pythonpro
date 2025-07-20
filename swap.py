f5e8c2fa84c0a66d57a544c39c5bb85fc2b03950
Vishwa1112 <boxervishwa1@gmail.com>
2025-07-20 06:46:29 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,13 @@
+a=int(input("enter (a) number"))
+b=int(input("enter (b) number"))
+c=int(input("enter (c) number"))
+print("value of a",a)
+print("value of b",b)
+print("value of c",c)
+temp=a
+a=b
+b=c
+c=temp
+print("value of a",a)
+print("value of b",b)
+print("value of c",c)
