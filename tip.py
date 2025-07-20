fe81fce336473b08c921057645f0ab4c3fb3d33b
Vishwa1112 <boxervishwa1@gmail.com>
2025-07-20 06:43:47 +0530

    Add files via upload

---
+++ tip.py
@@ -0,0 +1,8 @@
+def restaurant_bill(bill_amount):
+    a=(bill_amount*10)/100
+    total_amount=a+bill_amount
+    print("total bill amount",total_amount)
+num=int(input("enter a bill amount"))
+restaurant_bill(num)
+
+     
